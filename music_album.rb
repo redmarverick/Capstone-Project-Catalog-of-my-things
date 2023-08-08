030b414942ddb78a1f7ec36131742cdf7746a865
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-08-08 09:06:36 -0800

    Add MusicAlbum class

---
+++ music_album.rb
@@ -0,0 +1,11 @@
+class MusicAlbum < Item
+  def initialize(genre = nil, author = nil, source = nil, label = nil, publish_date = nil, archived: false, on_spotify: false)
+    super()
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?()
+    return true if on_spotify && archived
+    return false
+  end
+end
