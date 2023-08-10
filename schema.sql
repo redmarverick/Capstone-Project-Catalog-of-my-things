83cd1a671565f7417f3fb325560b29a579c800e0
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-08-10 11:12:31 -0300

    Create tables for items: books, music albums, and games

--- schema.sql
+++ schema.sql
@@ -13,3 +13,31 @@ CREATE TABLE authors (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL
 );
+
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY,
+  label_id INTEGER REFERENCES labels(id),
+  genre_id INTEGER REFERENCES genres(id),
+  author_id INTEGER REFERENCES authors(id),
+  published_date DATE NOT NULL,
+  publisher TEXT NOT NULL,
+  cover_state TEXT NOT NULL
+);
+
+CREATE TABLE music_albums (
+  id SERIAL PRIMARY KEY,
+  label_id INTEGER REFERENCES labels(id),
+  genre_id INTEGER REFERENCES genres(id),
+  author_id INTEGER REFERENCES authors(id),
+  published_date DATE NOT NULL,
+  on_spotify BOOLEAN NOT NULL
+);
+
+CREATE TABLE games (
+  id SERIAL PRIMARY KEY,
+  label_id INTEGER REFERENCES labels(id),
+  genre_id INTEGER REFERENCES genres(id),
+  author_id INTEGER REFERENCES authors(id),
+  published_date DATE NOT NULL,
+  last_played_at DATE NOT NULL
+);
