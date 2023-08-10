485a1a936af259e391d0071cfc5fa12e21172462
redmarverick <nyldrift@hotmail.com>
2023-08-10 11:58:13 -0300

    Update schema.sql
    - change id to uuid

--- schema.sql
+++ schema.sql
@@ -1,43 +1,45 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE labels (
-  id SERIAL PRIMARY KEY,
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   title TEXT NOT NULL,
   color TEXT NOT NULL
 );
 
 CREATE TABLE genres (
-  id SERIAL PRIMARY KEY,
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   name TEXT NOT NULL
 );
 
 CREATE TABLE authors (
-  id SERIAL PRIMARY KEY,
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   name TEXT NOT NULL
 );
 
 CREATE TABLE books (
-  id SERIAL PRIMARY KEY,
-  label_id INTEGER REFERENCES labels(id),
-  genre_id INTEGER REFERENCES genres(id),
-  author_id INTEGER REFERENCES authors(id),
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  label_id UUID REFERENCES labels(id),
+  genre_id UUID REFERENCES genres(id),
+  author_id UUID REFERENCES authors(id),
   published_date DATE NOT NULL,
   publisher TEXT NOT NULL,
   cover_state TEXT NOT NULL
 );
 
 CREATE TABLE music_albums (
-  id SERIAL PRIMARY KEY,
-  label_id INTEGER REFERENCES labels(id),
-  genre_id INTEGER REFERENCES genres(id),
-  author_id INTEGER REFERENCES authors(id),
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  label_id UUID REFERENCES labels(id),
+  genre_id UUID REFERENCES genres(id),
+  author_id UUID REFERENCES authors(id),
   published_date DATE NOT NULL,
   on_spotify BOOLEAN NOT NULL
 );
 
 CREATE TABLE games (
-  id SERIAL PRIMARY KEY,
-  label_id INTEGER REFERENCES labels(id),
-  genre_id INTEGER REFERENCES genres(id),
-  author_id INTEGER REFERENCES authors(id),
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  label_id UUID REFERENCES labels(id),
+  genre_id UUID REFERENCES genres(id),
+  author_id UUID REFERENCES authors(id),
   published_date DATE NOT NULL,
   last_played_at DATE NOT NULL
 );
